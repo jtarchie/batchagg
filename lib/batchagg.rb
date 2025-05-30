c3684f1bace0a9ba78c0389a7eb0d5c6f6f2079e
JT Archie <jtarchie@gmail.com>
2025-05-29 19:35:34 -0600

    refactor

--- lib/batchagg.rb
+++ lib/batchagg.rb
@@ -1,72 +1,119 @@
-require 'ostruct'
+require "ostruct"
 
 module BatchAgg
+  class AggregateDefinition
+    attr_reader :name, :type, :block
+
+    def initialize(name, type, block)
+      @name = name
+      @type = type
+      @block = block
+    end
+  end
+
+  class QueryBuilder
+    def initialize(base_model)
+      @base_model = base_model
+    end
+
+    def build_query_for_record(record, aggregates)
+      main_table = @base_model.arel_table
+      projections = build_projections(record, aggregates)
+
+      main_table
+        .project(*projections)
+        .where(main_table[:id].eq(record.id))
+    end
+
+    private
+
+    def build_projections(record, aggregates)
+      aggregates.map do |aggregate|
+        build_projection_for_aggregate(record, aggregate)
+      end
+    end
+
+    def build_projection_for_aggregate(record, aggregate)
+      relation = aggregate.block.call(record)
+      subquery = build_count_subquery(relation)
+      Arel.sql("(#{subquery})").as(aggregate.name.to_s)
+    end
+
+    def build_count_subquery(relation)
+      relation.except(:select).select("COUNT(*)").to_sql
+    end
+  end
+
+  class AggregateResult
+    def initialize(record, aggregates, base_model)
+      @record = record
+      @aggregates = aggregates
+      @query_builder = QueryBuilder.new(base_model)
+      @results = execute_query
+    end
+
+    attr_reader :record
+
+    def get_aggregate_value(name)
+      @results[name.to_s]
+    end
+
+    private
+
+    def execute_query
+      query = @query_builder.build_query_for_record(@record, @aggregates)
+      result = ActiveRecord::Base.connection.select_all(query.to_sql)
+      OpenStruct.new(result.first)
+    end
+  end
+
+  class AggregateResultClass
+    def initialize(aggregates, base_model)
+      @aggregates = aggregates
+      @base_model = base_model
+    end
+
+    def new(record)
+      result = AggregateResult.new(record, @aggregates, @base_model)
+      add_aggregate_methods(result)
+      result
+    end
+
+    private
+
+    def add_aggregate_methods(result)
+      @aggregates.each do |aggregate|
+        define_aggregate_method(result, aggregate.name)
+      end
+    end
+
+    def define_aggregate_method(result, name)
+      result.define_singleton_method(name) do
+        get_aggregate_value(name)
+      end
+    end
+  end
+
   class AggregateBuilder
     def initialize(base_model)
       @base_model = base_model
-      @aggregates = {}
+      @aggregates = []
     end
 
     def count(name, &block)
-      @aggregates[name] = { type: :count, block: block }
+      aggregate = AggregateDefinition.new(name, :count, block)
+      @aggregates << aggregate
     end
 
     def build_class
-      aggregates = @aggregates
-      base_model = @base_model
-
-      Class.new do
-        attr_reader :record
-
-        def initialize(record)
-          @record = record
-          @results = self.class.execute_query(record)
-        end
-
-        aggregates.each do |name, config|
-          define_method(name) do
-            @results[name.to_s]
-          end
-        end
-
-        define_singleton_method :execute_query do |record|
-          # Build the main query using Arel
-          main_table = base_model.arel_table
-
-          # Create subquery projections for each aggregate
-          projections = aggregates.map do |name, config|
-            relation = config[:block].call(record) # This is an ActiveRecord::Relation
-
-            # Get the ActiveRecord::Relation for the subquery count
-            # This relation will have its own context for bind parameters (e.g., from record.id)
-            subquery_count_relation = relation.except(:select).select('COUNT(*)')
-
-            # Generate SQL for the subquery; ActiveRecord resolves its bind parameters here
-            subquery_sql = subquery_count_relation.to_sql
-
-            # Create a named projection using Arel.sql to treat the subquery SQL as a literal
-            # and then alias it with .as()
-            Arel.sql("(#{subquery_sql})").as(name.to_s)
-          end
-
-          # Build the main query
-          query = main_table
-            .project(*projections)
-            .where(main_table[:id].eq(record.id))
-
-          # Execute the query
-          result = ActiveRecord::Base.connection.select_all(query.to_sql)
-
-          return OpenStruct.new(result.first)
-        end
-      end
+      AggregateResultClass.new(@aggregates, @base_model)
     end
   end
 end
 
 def aggregate(&block)
-  # Infer the base model from the block context
-  # For now, we'll assume User as the base model
+  # For now, assume User as the base model
+  # In the future, this could be made configurable
   builder = BatchAgg::AggregateBuilder.new(User)
   builder.instance_eval(&block)
   builder.build_class
